708570544473bc2e74d9601d403c4a8db5b29db6
hemanthtv <52783849+hemanthtv@users.noreply.github.com>
2020-09-15 14:26:31 +0530

    Source code

--- codepython.py
+++ codepython.py
@@ -16,18 +16,16 @@ for root,_,files in os.walk(paths):
         file = os.path.join(root,filename)
         if fnmatch.fnmatch(file,'*script.jpg'):
             label = file
-            #print("label=",label)
             test = cv2.imread(label)        
             encodings_tobe_Check= fr.face_encodings(test)[0]
                     
         elif fnmatch.fnmatch(file,"*.jpg"):
             image = file
-            #print("image=",image)
             img = cv2.imread(image)
             facesCurFrame = fr.face_locations(img)
             Known_encoding_values= fr.face_encodings(img, facesCurFrame)
         
-        #print(file)
+        
     matchinglist = fr.compare_faces(Known_encoding_values, encodings_tobe_Check)  
     if matchinglist == []:
         continue
